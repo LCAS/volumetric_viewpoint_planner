191e3554ae09dec715295d6954499c9c38ed1ba4
Abdurrahman Yilmaz <yilmazabdurrah@itu.edu.tr>
2024-11-14 17:34:14 +0000

    RViz installation and ROS_DOMAIN_ID assign

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,9 +7,13 @@ function add_config_if_not_exist {
     fi
 }
 
+add_config_if_not_exist "export ROS_DOMAIN_ID=9"
+
 add_config_if_not_exist "source /opt/ros/humble/setup.bash"
 LOCAL_SETUP_FILE=`pwd`/install/setup.bash
 add_config_if_not_exist "if [ -r $LOCAL_SETUP_FILE ]; then source $LOCAL_SETUP_FILE; fi"
+
+source $HOME/.bashrc
 
 source /opt/ros/humble/setup.bash
 colcon build --symlink-install --continue-on-error
